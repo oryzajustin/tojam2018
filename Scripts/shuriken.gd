cb7274e2d0eb95b8011c40785940416c2fa02e88
Victor Shi <victoryshoe@gmail.com>
2018-05-05 21:54:23 -0400

    Add trap and functionality

--- Scripts/shuriken.gd
+++ Scripts/shuriken.gd
@@ -1,30 +1,30 @@
 extends KinematicBody2D
 
-var vel = Vector2(0, 0)
-export var speed = 380
+var velocity = Vector2(0, 0)
+export var speed = 400
 
 func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	var collision_info = move_and_collide(vel * delta)
+	var collision_info = move_and_collide(velocity * delta)
 	if collision_info: # True after collision
-		print("Hit something...")
-		vel = Vector2(0, 0) # Stop shuriken movement
 		if collision_info.collider.is_in_group("players"):
-			print("Hit a guy!!!")
-			collision_info.collider.death()
 			# Could do a screen shake here
-			# Projectile should continue
-			vel = Vector2(speed, 0).rotated(rotation)
+			collision_info.collider.death()
+			# Projectile continues flying
+			velocity = Vector2(speed, 0).rotated(rotation)
+		else:
+			velocity = Vector2(0, 0) # Stop shuriken movement
+			get_node("CollisionPolygon2D").disabled = true # Allow players to move through stopped shuriken (if they can't pick up)
 
 func start_at(dir, pos):
 	rotation = dir
 	position = pos
-	vel = Vector2(speed, 0).rotated(dir)
+	velocity = Vector2(speed, 0).rotated(dir)
 
 func _on_Pickup_Radius_body_entered(body):
-	if vel == Vector2(0, 0): # Check if shuriken is stationary
+	if velocity == Vector2(0, 0): # Check if shuriken is stationary
 		if body.is_in_group("players"):
 			body.pickup_shuriken(self)
 
--- Scripts/trap.gd
+++ Scripts/trap.gd
@@ -1,16 +1,45 @@
-extends Area2D
+extends Node2D
 
-
+var visible_player_counter = 0
+var defused = false
+onready var defuse_timer = get_node("Defuse Timer")
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
 func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-	pass
-
+	if defused and defuse_timer.get_time_left() == 0:
+		self.queue_free()
+	
 func set_trap(pos):
 	position = pos
+
+func _on_Trigger_Radius_body_entered(body):
+	if body.is_in_group("players"):
+			body.trigger_trap()
+			self.queue_free()
+
+func _on_Defuse_Radius_body_entered(body):
+	if body.is_in_group("players"):
+		defused = true
+		defuse_timer.start()
+
+
+func _on_Defuse_Radius_body_exited(body):
+	if body.is_in_group("players"):
+		defused = false
+		defuse_timer.stop()
+
+
+func _on_Visible_Radius_body_entered(body):
+	if body.is_in_group("players"):
+		visible_player_counter += 1
+		self.show()
+
+
+func _on_Visible_Radius_body_exited(body):
+	if body.is_in_group("players"):
+		if visible_player_counter > 0:
+			visible_player_counter -= 1
+		if visible_player_counter == 0:
+			self.hide()
